c02d517484ea2820771d4379d116daa9e077c9a0
Prashant4171992 <43987137+Prashant4171992@users.noreply.github.com>
2018-12-28 19:43:17 +0530

    Application Insights Enabled...

--- LowesLinkTemplates/App_Start/FilterConfig.cs
+++ LowesLinkTemplates/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace LowesLinkTemplates
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
